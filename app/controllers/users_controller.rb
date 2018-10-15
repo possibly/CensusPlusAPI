18ad8620803ca57df3bc99bc210ec5b3fa4ef4a3
Tyler Brothers <tylerbrothers1@gmail.com>
2018-10-14 22:35:36 -0600

    Add census_plus_data to User view and controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :census_plus_data)
     end
 end
